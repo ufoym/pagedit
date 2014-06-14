8cdb70a328f9ac772ce31b46457d06c1b62d3e37
Ming <a@ufoym.com>
2014-06-14 19:39:08 +0800

    Reorganize code

--- app.py
+++ app.py
@@ -1,68 +1,36 @@
  # -*- coding: utf-8 -*
 import os
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
+# ----------------------------------------------------------------------------
+# settings
 charset = 'utf-8'
 content_path = 'content'
-if not os.path.exists(content_path):
-    os.mkdir(content_path)
-
-def get_template(name, content = 'Write something here...'):
-    return ''.join(['''
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
-    <script type="text/javascript">
-    tinymce.init({
-        selector: "div.editable",
-        inline: true,
-        plugins: [
-            "autoresize",
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
-        ],
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-    });
-    </script>
-    </head>
-    <body>
-    <form id="main-form" method="post" action="/''',
-    name,
-    '''">
-    <div id="content" class="editable" onblur="document.getElementById('main-form').submit()">''',
-    content.decode(charset),
-    '''
-    </div>
-    </form>
-    </body>
-    </html>'''])
+# ----------------------------------------------------------------------------
 
 @app.route("/<path:name>", methods=['GET', 'POST'])
 def page(name):
     fn = os.path.join(content_path, name + '.txt')
-    template = ''
+    content = 'Write something here...'
     if request.method == 'GET':
         if os.path.exists(fn):
             with open(fn, 'r') as f:
-                template = get_template(name, f.read())
-        else:
-            template = get_template(name)
+                content = f.read().decode(charset)
     else:
-        content = request.form['content'].encode(charset)
+        content = request.form['content']
         name_terms = name.split('/')
         if len(name_terms) > 1:
             folder = '/'.join([content_path] + name_terms[:-1])
             if not os.path.exists(folder):
                 os.makedirs(folder)
         with open(fn, 'w') as f:
-            f.write(content)
-        template = get_template(name, content)
-    return template
+            f.write(content.encode(charset))
+    return render_template('page.html', name = name, content = content)
+
+# ----------------------------------------------------------------------------
 
 if __name__ == "__main__":
+    if not os.path.exists(content_path):
+        os.mkdir(content_path)
     app.run(debug=True)
