67ec2289ac5921d383e257b54670a28edc9271a7
Ming <a@ufoym.com>
2014-06-19 23:52:13 +0800

    MINOR

--- do.py
+++ do.py
@@ -4,8 +4,13 @@ from flask import Flask, request, redirect, url_for, \
         render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'uploads'
+# ----------------------------------------------------------------------------
+
+VAR_FOLDER = 'var'
+UPLOAD_FOLDER = os.path.join('var', 'uploads')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+# ----------------------------------------------------------------------------
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(
@@ -15,10 +20,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/")
-def hello():
-    return render_template('page.html')
-
 @app.route('/_/upload', methods=['POST'])
 def upload_file():
     funcnum = request.args.get('CKEditorFuncNum', 0, type=str)
@@ -27,15 +28,18 @@ def upload_file():
     file = request.files.get('upload')
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        folder = app.config['UPLOAD_FOLDER']
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        file.save(os.path.join(folder, filename))
         file_url = url_for('uploaded_file', filename=filename)
         message = ''
-    return '<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction(%s, "%s", "%s")</script>' % (funcnum, file_url, message)
+    return '<script type="text/javascript">window.parent.CKEDITOR.tools' \
+       '.callFunction(%s, "%s", "%s")</script>' % (funcnum, file_url, message)
 
 @app.route('/_/upload/<filename>', methods=['GET'])
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/_/upload', methods=['GET'])
 def uploaded_file_list():
@@ -54,8 +58,11 @@ def uploaded_file_list():
             info_list.append(info)
     return json.dumps(info_list)
 
+# ----------------------------------------------------------------------------
+
+@app.route("/")
+def page():
+    return render_template('page.html')
+
 if __name__ == "__main__":
-    for folder in (UPLOAD_FOLDER,):
-        if not os.path.exists(folder):
-            os.makedirs(folder)
     app.run(debug=True)
